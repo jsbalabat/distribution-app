a342d0c09c3a85c9a24d1349b5f20d5f5e2cddbd
Marc <johnfelixmarc@gmail.com>
2026-06-17 14:42:50 +0800

    fix: redirect android build output to build dir

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,6 +20,19 @@ allprojects {
     }
 }
 
+// Redirect every module's build output to the project-root `build/` folder, which
+// is where the Flutter tool looks for the generated APK.
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
