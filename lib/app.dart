7f084bb17cab6c60960b78df746721d640b973d1
Marc <johnfelixmarc@gmail.com>
2026-08-30 10:32:31 +0800

    chore: rename the app to Ledgerly and add a proper PWA manifest

--- lib/app.dart
+++ lib/app.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sales Field App',
+      title: 'Ledgerly',
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
